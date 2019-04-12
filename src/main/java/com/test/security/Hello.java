6776199b6cc19cfba22c3105be4ce162feeca9ed
Mike <zhangrunwei@aliyun.com>
2019-04-13 03:20:01 +0800

    delete security dir

--- src/main/java/com/test/security/Hello.java
+++ src/main/java/com/test/security/Hello.java
@@ -10,14 +10,8 @@ import java.io.IOException;
 
 @WebServlet("/hello")
 public class Hello extends HttpServlet {
-    // 直接返回
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.getWriter().println("Hello, 2019!");
     }
-    // 转发 World
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/world");
-        dispatcher.forward(request, response);
-    }
-
 }
--- src/main/webapp/error.jsp
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-    <title>出错了</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-</head>
-<body>
-    <p>Sorry!</p>
-</body>
-</html>
